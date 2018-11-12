14041bc3b371f20c33661c2c55c8000c5f47316a
Дмитрий Петров <dartwen@yandex.ru>
2018-11-13 02:31:42 +0300

    done task 1

--- func-component/films/js/Stars.js
+++ func-component/films/js/Stars.js
@@ -1,5 +1,23 @@
 'use strict';
 
-function Stars() {
-  return <ul className="card-body-stars u-clearfix"><li><Star /></li></ul>;
+/**
+ * @return {boolean}
+ */
+function Stars({count}) {
+    return (count >= 1 && count <= 5) ? <ul
+        className="card-body-stars u-clearfix">{calculationStars(count)}</ul> : false;
 }
+
+Stars.defaultProps = {stars: 0};
+
+function calculationStars(count) {
+    let itemStar = [];
+    let i = 0;
+    while (i !== count) {
+        itemStar.push(<li key={i}><Star/></li>);
+        i++;
+    }
+
+    return itemStar;
+}
+
