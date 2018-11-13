6479a5339a4077ae2ee7079a2107f414966c4784
Дмитрий Петров <dartwen@yandex.ru>
2018-11-13 03:00:22 +0300

    done task 2

--- func-component/chat/js/MessageHistory.js
+++ func-component/chat/js/MessageHistory.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function MessageHistory({list}) {
+    const messageHistory = list.map(message => {
+        if (message.type === 'response') {
+            return <Response from={message.from} message={message} />
+        }
+        if (message.type === 'message'){
+            return <Message from={message.from} message={message} />
+        }
+        if (message.type === 'typing'){
+            return <Typing from={message.from} message={message} />
+        }
+    });
+
+    return (<ul>{messageHistory}</ul>)
+
+}
+
+MessageHistory.defaultProps = {list: []};
