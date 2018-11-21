fa0469f170884efadbb9aa8126101ec7534235ae
Дмитрий Петров <dartwen@yandex.ru>
2018-11-21 12:25:02 +0300

    done task 1

--- state/filter/js/App.js
+++ state/filter/js/App.js
@@ -1,11 +1,40 @@
-'use strict'
+'use strict';
 
-const App = props => (
-  <div>
-    <Toolbar
-      filters={props.filters}
-      selected={'All'}
-      onSelectFilter={(filter) => console.log(filter)} />
-    <Portfolio projects={props.projects} />
-  </div>
-);
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            filters: 'All'
+        };
+    }
+
+
+    handleClickFilters = (filter) => {
+        this.setState({
+            filters: filter
+        })
+    };
+
+    projectsGet = (projects, filters) => {
+        if (filters === 'All') return projects;
+        let projectsFilters = [];
+        projects.map((item) => {
+            item.category === filters ? projectsFilters.push(item) : '';
+        });
+
+        return projectsFilters;
+    };
+
+    render() {
+        const {filters} = this.state;
+
+        return (<div>
+                <Toolbar
+                    filters={this.props.filters}
+                    selected={filters}
+                    onSelectFilter={this.handleClickFilters}/>
+                <Portfolio projects={this.projectsGet(this.props.projects, filters)}/>
+            </div>
+        );
+    }
+}
