7b0eed664bf6f359d83f820cc34a7847ac567f61
Дмитрий Петров <dartwen@yandex.ru>
2018-12-03 11:51:18 +0300

    task done menu

---
+++ routing/menu/js/Menu.js
@@ -0,0 +1,13 @@
+class Menu extends React.Component {
+    render() {
+        const {activeClassName}=this.props;
+        return (
+            <nav className="menu">
+                <NavLink to="/" exact className="menu__item" activeClassName={activeClassName}>Главная</NavLink>
+                <NavLink to="/drift" className="menu__item" activeClassName={activeClassName}>Дрифт-такси</NavLink>
+                <NavLink to="/timeattack" className="menu__item" activeClassName={activeClassName}>Time Attack</NavLink>
+                <NavLink to="/forza" className="menu__item" activeClassName={activeClassName}>Forza Karting</NavLink>
+            </nav>
+        );
+    }
+}
